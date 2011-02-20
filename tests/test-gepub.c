1515f32d7857776a1bcf5f5bd8dd8a2a385990b8
danigm <danigm@wadobo.com>
2011-02-20 20:49:59 +0100

    Test shows webkit and test output

--- tests/test-gepub.c
+++ tests/test-gepub.c
@@ -1,9 +1,22 @@
+#include <string.h>
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <libgepub/gepub.h>
 #include <webkit/webkit.h>
 
-#define TEST(f,arg) printf ("\n### TESTING " #f " ###\n\n"); f (arg); printf ("\n\n");
+gchar *buf = NULL;
+gchar *buf2 = NULL;
+gchar *tmpbuf;
+
+#define PTEST1(...) printf (__VA_ARGS__)
+#define PTEST2(...) buf = g_strdup_printf (__VA_ARGS__);\
+                    tmpbuf = buf2;\
+                    buf2 = g_strdup_printf ("%s%s", buf2, buf);\
+                    g_free (buf);\
+                    g_free (tmpbuf)
+#define PTEST PTEST2
+
+#define TEST(f,arg) PTEST ("\n### TESTING " #f " ###\n\n"); f (arg); PTEST ("\n\n");
 
 void
 test_open (const char *path)
@@ -16,9 +29,9 @@ test_open (const char *path)
     a = gepub_archive_new (path);
     list_files = gepub_archive_list_files (a);
     size = g_list_length (list_files);
-    printf ("%d\n", size);
+    PTEST ("%d\n", size);
     for (i = 0; i < size; i++) {
-        printf ("file: %s\n", (char *)g_list_nth_data (list_files, i));
+        PTEST ("file: %s\n", (char *)g_list_nth_data (list_files, i));
         g_free (g_list_nth_data (list_files, i));
     }
 
@@ -47,7 +60,7 @@ test_read (const char *path)
     file = g_list_nth_data (list_files, 0);
     gepub_archive_read_entry (a, file, &buffer, &bufsize);
     if (bufsize)
-        g_printf ("doc:%s\n----\n%s\n-----\n", file, buffer);
+        PTEST ("doc:%s\n----\n%s\n-----\n", file, buffer);
 
     g_free (buffer);
 
@@ -66,7 +79,7 @@ test_root_file (const char *path)
     a = gepub_archive_new (path);
 
     root_file = gepub_archive_get_root_file (a);
-    printf ("root file: %s\n", root_file);
+    PTEST ("root file: %s\n", root_file);
     if (root_file)
         g_free (root_file);
 
@@ -88,11 +101,11 @@ test_doc_name (const char *path)
     gchar *author = gepub_doc_get_metadata (doc, META_AUTHOR);
     gchar *description = gepub_doc_get_metadata (doc, META_DESC);
 
-    printf ("title: %s\n", title);
-    printf ("author: %s\n", author);
-    printf ("id: %s\n", id);
-    printf ("lang: %s\n", lang);
-    printf ("desc: %s\n", description);
+    PTEST ("title: %s\n", title);
+    PTEST ("author: %s\n", author);
+    PTEST ("id: %s\n", id);
+    PTEST ("lang: %s\n", lang);
+    PTEST ("desc: %s\n", description);
 
     g_free (title);
     g_free (lang);
@@ -105,19 +118,19 @@ test_doc_name (const char *path)
 void
 pk (gchar *key, gchar *value, gpointer data)
 {
-    printf ("%s: %s\n", key, value);
+    PTEST ("%s: %s\n", key, value);
 }
 
 void
 test_doc_resources (const char *path)
 {
     GEPUBDoc *doc = gepub_doc_new (path);
-    GHashTable *ht = gepub_doc_get_resources (doc);
-    g_hash_table_foreach (ht, pk, NULL);
+    GHashTable *ht = (GHashTable*)gepub_doc_get_resources (doc);
+    g_hash_table_foreach (ht, (GHFunc)pk, NULL);
     guchar *ncx;
 
     ncx = gepub_doc_get_resource (doc, "ncx");
-    printf ("ncx:\n%s\n", ncx);
+    PTEST ("ncx:\n%s\n", ncx);
     g_free (ncx);
 
     g_object_unref (doc);
@@ -127,7 +140,7 @@ void
 p (gchar *value, gpointer data)
 {
     static int id = 0;
-    printf ("%d: %s\n", id++, value);
+    PTEST ("%d: %s\n", id++, value);
 }
 
 void
@@ -147,7 +160,12 @@ main (int argc, char **argv)
     gtk_init (&argc, &argv);
 
     GtkWidget *window;
+    GtkWidget *vpaned;
+    GtkWidget *textview;
     GtkWidget *wview;
+    GtkWidget *scrolled;
+
+    GtkTextBuffer *buffer;
 
     if (argc < 2) {
         printf ("you should provide an .epub file\n");
@@ -155,12 +173,26 @@ main (int argc, char **argv)
     }
 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    //wview = webkit_web_view_new ();
+    g_signal_connect (window, "destroy", (GCallback)gtk_exit, NULL);
+    vpaned = gtk_hpaned_new ();
+    gtk_container_add (GTK_CONTAINER (window), vpaned);
 
-    //gtk_container_add (GTK_CONTAINER (window), wview);
-    //gtk_widget_show_all (window);
+    wview = webkit_web_view_new ();
+    scrolled = gtk_scrolled_window_new (NULL, NULL);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_container_add (GTK_CONTAINER (scrolled), wview);
+    gtk_widget_set_size_request (wview, 500, 300);
+    gtk_paned_add1 (GTK_PANED (vpaned), scrolled);
 
-    //webkit_web_view_open (WEBKIT_WEB_VIEW (wview), "http://google.es");
+    textview = gtk_text_view_new ();
+    scrolled = gtk_scrolled_window_new (NULL, NULL);
+    gtk_container_add (GTK_CONTAINER (scrolled), textview);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_paned_add2 (GTK_PANED (vpaned), scrolled);
+
+    gtk_widget_show_all (window);
+
+    webkit_web_view_open (WEBKIT_WEB_VIEW (wview), "http://google.es");
 
     TEST(test_open, argv[1])
     TEST(test_read, argv[1])
@@ -168,6 +200,12 @@ main (int argc, char **argv)
     TEST(test_doc_name, argv[1])
     TEST(test_doc_resources, argv[1])
     TEST(test_doc_spine, argv[1])
+
+    if (buf2) {
+        buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (textview));
+        gtk_text_buffer_set_text (buffer, buf2, strlen (buf2));
+        g_free (buf2);
+    }
 
     gtk_main ();
 
